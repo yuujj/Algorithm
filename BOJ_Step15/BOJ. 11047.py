f9c7ea1cc66b46b137857e16a192336d435bbca6
yuujj <yujj0108@gmail.com>
2022-04-14 13:36:08 +0900

    동전0

---
+++ BOJ_Step15/BOJ. 11047.py
@@ -0,0 +1,14 @@
+### 동전0 ###
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+coins = [coin for coin in coins if coin <= k]
+cnt = 0
+while coins:
+    coin = coins.pop()
+    mod, re = k // coin, k % coin
+    cnt += mod
+    k = re
+print(cnt)
